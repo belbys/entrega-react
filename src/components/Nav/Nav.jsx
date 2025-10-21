980fec62ac97d8ed30901d133a3b0cdaaa3774e5
marina <pepahola.mm@gmail.com>
2025-10-20 22:53:12 -0300

    arreglo de logo de mierda

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ export const Nav = () => {
   
   return (
     <nav className="navbar">
-      <h1 className="logo">Planeta Creativo</h1>
+      <img src="/public/images/logo.png" alt="Planeta Creativo" className="logo-img" />
       <ul className="nav-list">
         <li> <Link to={"/"}>Home</Link> </li>
         <li> <Link to={"/category/stickers"}>Stickers</Link> </li>
